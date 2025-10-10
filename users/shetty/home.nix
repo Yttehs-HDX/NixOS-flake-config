a5ff4d00367d63817838e764a3c973b793326ecf
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-10-11 00:53:41 +0800

    feat(home/hyprland): setup hyprland ui

--- users/shetty/home.nix
+++ users/shetty/home.nix
@@ -188,7 +188,6 @@ in {
 
   wayland.windowManager.hyprland = let
     terminal = "kitty";
-    fileManager = "yazi";
   in {
     enable = true;
     xwayland.enable = true;
@@ -218,7 +217,6 @@ in {
       bind = [
         # Basic
         "$mod, Q, exec, ${terminal}"
-        "$mod, E, exec, ${fileManager}"
         "$mod, R, exec, wofi --show drun"
         "$mod, V, exec, hyprctl dispatch togglefloating"
         "$mod, F, fullscreen"
@@ -261,6 +259,58 @@ in {
           ) 8
         )
       );
+
+      general = {
+        gaps_in = 4;
+        gaps_out = 9;
+        border_size = 2;
+        "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
+        "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+        resize_on_border = true;
+        layout = "dwindle";
+      };
+
+      decoration = {
+        shadow.enabled = false;
+        rounding = 10;
+        dim_special = 0.3;
+        blur = {
+          enabled = true;
+          special = true;
+          size = 6; # 6
+          passes = 2; # 3
+          new_optimizations = true;
+          ignore_opacity = true;
+          xray = false;
+        };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = [
+          "linear, 0, 0, 1, 1"
+          "md3_standard, 0.2, 0, 0, 1"
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+          "md3_accel, 0.3, 0, 0.8, 0.15"
+          "overshot, 0.05, 0.9, 0.1, 1.1"
+          "crazyshot, 0.1, 1.5, 0.76, 0.92"
+          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+          "fluent_decel, 0.1, 1, 0, 1"
+          "easeInOutCirc, 0.85, 0, 0.15, 1"
+          "easeOutCirc, 0, 0.55, 0.45, 1"
+          "easeOutExpo, 0.16, 1, 0.3, 1"
+        ];
+        animation = [
+          "windows, 1, 3, md3_decel, popin 60%"
+          "border, 1, 10, default"
+          "fade, 1, 2.5, md3_decel"
+          # "workspaces, 1, 3.5, md3_decel, slide"
+          "workspaces, 1, 3.5, easeOutExpo, slide"
+          # "workspaces, 1, 7, fluent_decel, slidefade 15%"
+          # "specialWorkspace, 1, 3, md3_decel, slidefadevert 15%"
+          "specialWorkspace, 1, 3, md3_decel, slidevert"
+        ];
+      };
     };
   };
 
