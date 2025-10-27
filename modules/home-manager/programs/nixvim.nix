d0150332d4d5822750647cc36eb7c38abdfaa80d
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-10-27 16:02:08 +0800

    feat(nixvim): impl catppuccin theme

--- modules/home-manager/programs/nixvim.nix
+++ modules/home-manager/programs/nixvim.nix
@@ -1,6 +1,36 @@
 { ... }:
 
 {
-  programs.nixvim = { enable = true; };
+  programs.nixvim = {
+    enable = true;
+
+    colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        disable_underline = true;
+        flavour = "mocha";
+        integrations = {
+          cmp = true;
+          gitsigns = true;
+          mini = {
+            enabled = true;
+            indentscope_color = "";
+          };
+          notify = false;
+          nvimtree = true;
+          treesitter = true;
+        };
+        styles = {
+          booleans = [ "bold" "italic" ];
+          conditionals = [ "bold" ];
+        };
+        term_colors = true;
+      };
+    };
+
+    plugins = {
+      lualine.enable = true;
+    };
+  };
 }
 
