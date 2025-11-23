85d9323277cd3ddb0841fb7704e05296d01bbc4a
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-11-23 16:11:40 +0800

    feat(home/waybar): use kanji icons for workspaces

--- modules/home-manager/programs/waybar.nix
+++ modules/home-manager/programs/waybar.nix
@@ -38,9 +38,17 @@
         disable-scroll = true;
         all-outputs = true;
         active-only = false;
-        format = "{id}";
+        format = "{icon}";
+        format-icons = {
+          "1" = "壱";
+          "2" = "弐";
+          "3" = "参";
+          "4" = "肆";
+          "5" = "伍";
+          "6" = "陸";
+        };
         on-click = "activate";
-        persistent-workspaces = { "*" = [ 1 2 3 4 5 ]; };
+        persistent-workspaces = { "*" = [ 1 2 3 4 5 6 ]; };
       };
       "hyprland/window" = {
         format = "{class}";
