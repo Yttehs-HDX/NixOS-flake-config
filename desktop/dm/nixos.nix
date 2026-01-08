7e7f5391a4339acfac32ac34d3e3e3d577c13fe1
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-01-08 17:45:27 +0800

    refactor: move SDDM configuration to a separate file for better organization

--- desktop/dm/nixos.nix
+++ desktop/dm/nixos.nix
@@ -1,25 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  services.displayManager.sddm = let
-    flavor = "mocha";
-    accent = "lavender";
-  in {
-    enable = true;
-    wayland.enable = true;
-    theme = "${
-        pkgs.catppuccin-sddm.override { inherit flavor accent; }
-      }/share/sddm/themes/catppuccin-${flavor}-${accent}";
-    package = pkgs.kdePackages.sddm;
-    extraPackages = with pkgs; [
-      kdePackages.qtsvg
-      kdePackages.qtmultimedia
-      kdePackages.qtvirtualkeyboard
-    ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    # Ensure the SDDM theme is available system-wide.
-    (catppuccin-sddm.override { flavor = "mocha"; })
+  imports = [
+    ./sddm/nixos.nix
   ];
 }
