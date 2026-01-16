e5ad58ddffd032e847e8977268f8e8fe7792fddd
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-01-16 21:52:59 +0800

    refactor(neovim): update configuration to use nixvim module and enhance plugin settings

--- home/software/neovim/default.nix
+++ home/software/neovim/default.nix
@@ -1,10 +1,11 @@
-{ lib, profile, ... }:
+{ lib, profile, nixvim, ... }:
 
 let
   mkSoftwarePackage = import ../_lib/mkSoftwarePackage.nix;
 in
 {
   imports = [
+    nixvim.homeModules.nixvim
     (mkSoftwarePackage {
       inherit lib profile;
       name = "neovim";
