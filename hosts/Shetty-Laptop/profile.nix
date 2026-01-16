024d5a3bbf3e2f586ae77f6576eff49fa5597020
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-01-17 02:13:53 +0800

    refactor(hosts): streamline system software configuration in profile.nix

--- hosts/Shetty-Laptop/profile.nix
+++ hosts/Shetty-Laptop/profile.nix
@@ -7,20 +7,18 @@
     system = "x86_64-linux";
   };
 
-  system = {
-    software = {
-      bluetooth.enable = true;
-      networking.enable = true;
-      firewall.enable = true;
-      grub.enable = true;
-      locale.enable = true;
-      nvidia.enable = true;
-      refind.enable = true;
-      asusctl.enable = true;
-      tlp.enable = true;
-      waydroid.enable = true;
-      zram.enable = true;
-      nix-ld.enable = true;
-    };
+  system.software = {
+    bluetooth.enable = true;
+    networking.enable = true;
+    firewall.enable = true;
+    grub.enable = true;
+    locale.enable = true;
+    nvidia.enable = true;
+    refind.enable = true;
+    asusctl.enable = true;
+    tlp.enable = true;
+    waydroid.enable = true;
+    zram.enable = true;
+    nix-ld.enable = true;
   };
 }
